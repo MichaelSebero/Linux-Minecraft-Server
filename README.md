48b15a9f13e599f99320d2ef84a4322786c0c2ef
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-06-15 14:53:19 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Linux-Minecraft-Server
- This script installs a Minecraft server for Linux.
+## Requirements
+. Arch-based system
+
+. AUR repo
+
+## How to Install
+```
+curl -LO https://raw.githubusercontent.com/MichaelSebero/Linux-Minecraft-Server/main/install.sh
+
+sh install.sh
+```
+. The system will reboot after the installation is over.
+
+## How to Use
+. Run Haguichi.
+```
+sudo haguichi
+```
+. Create a network & copy the network's IPv4 address.
+
+. Input the IPv4 address into `server.properties` under `server-ip=`
+
+. Start the server from the commands below.
+
+### Forge
+```
+cd /home/$USER/Minecraft_Server && java -Xmx4G -Xmx8G -jar forge.jar nogui
+```
+
+### Vanilla
+
+```
+cd /home/$USER/Minecraft_Server && java -Xmx4G -Xmx8G -jar server.jar nogui
+```
+
+## Important
+. Depending on your system you might need to disable your firewall if you plan on using Haguichi, it uses a LAN connection which some firewalls block.
+
+. You'll have to invite your friends to your Haguichi network "it's called Hamachi on Windows" and provide them with the network's IPv4 address to connect with the server.
+
+. Your IPv4 address will look something like this `25.49.96.2` or `25.49.89.92`.
+
+. If you have multiple versions of Java installed you'll need to remove one of them to get the server running correctly. `jdk8-openjdk` is used for 1.12 and `jdk17-openjdk` is used for modern versions.
+
+. Each Forge version requires the latest version.
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+su -c '
+# SCRIPT VOCABULARY
+pre_peak_minecraft_forge_server_jar="https://maven.minecraftforge.net/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10/forge-1.7.10-10.13.4.1614-1.7.10-installer.jar"
+peak_minecraft_forge_server_jar="https://maven.minecraftforge.net/net/minecraftforge/forge/1.12.2-14.23.5.2860/forge-1.12.2-14.23.5.2860-installer.jar"
+newest_minecraft_server_jar="https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar"
+eula_auto_agree="https://github.com/MichaelSebero/Linux-Minecraft-Server/raw/main/files/eula.txt"
+telemetry_remover="https://github.com/MichaelSebero/Linux-Minecraft-Server/raw/main/files/server.properties"
+
+# INSTALLATION
+pacman -S wget haguichi --noconfirm --needed && mkdir /home/$USER/Minecraft_Server && cd /home/$USER/Minecraft_Server
+
+echo "1. Minecraft Forge 1.7.10"
+echo "2. Minecraft Forge 1.12"
+echo "3. Minecraft Vanilla Latest"
+read -p "Enter your choice (1, 2 or 3): " choice
+
+if [ "$choice" = "1" ]; then
+    pacman -S jdk8-openjdk --noconfirm --needed && wget "$pre_peak_minecraft_forge_server_jar" && wget "$eula_auto_agree" && wget "$telemetry_remover" && java -jar forge-1.7.10-10.13.4.1614-1.7.10-installer.jar --installServer && mv forge-1.7.10-10.13.4.1614-1.7.10-universal.jar forge.jar && rm forge-1.7.10-10.13.4.1614-1.7.10-installer.jar && mkdir mods && mkdir config
+elif [ "$choice" = "2" ]; then
+    pacman -S jdk8-openjdk --noconfirm --needed && wget "$peak_minecraft_forge_server_jar" && wget "$eula_auto_agree" && wget "$telemetry_remover" && java -jar forge-1.12.2-14.23.5.2860-installer.jar --installServer && mv forge-1.12.2-14.23.5.2860.jar forge.jar && rm forge-1.12.2-14.23.5.2860-installer.jar && mkdir mods && mkdir config
+elif [ "$choice" = "3" ]; then
+    pacman -S jdk17-openjdk --noconfirm --needed && wget "$newest_minecraft_server_jar" && wget "$eula_auto_agree" && wget "$telemetry_remover"
+fi
+
+chmod 777 /home/$USER/Minecraft_Server -R && reboot'
