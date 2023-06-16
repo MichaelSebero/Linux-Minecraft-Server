d137e15d72ca6179b93c1a8680f402a9e0c5aca9
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-06-16 14:51:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ eula_auto_agree="https://github.com/MichaelSebero/Linux-Minecraft-Server/raw/mai
 telemetry_remover="https://github.com/MichaelSebero/Linux-Minecraft-Server/raw/main/files/server.properties"
 
 # INSTALLATION
-pacman -S wget haguichi --noconfirm --needed && mkdir /home/$USER/Minecraft_Server && cd /home/$USER/Minecraft_Server
+pacman -S haguichi --noconfirm --needed && mkdir /home/$USER/Minecraft_Server && cd /home/$USER/Minecraft_Server
 
 echo "1. Minecraft Forge 1.7.10"
 echo "2. Minecraft Forge 1.12"
@@ -17,11 +17,11 @@ echo "3. Minecraft Vanilla Latest"
 read -p "Enter your choice (1, 2 or 3): " choice
 
 if [ "$choice" = "1" ]; then
-    pacman -S jdk8-openjdk --noconfirm --needed && wget "$pre_peak_minecraft_forge_server_jar" && wget "$eula_auto_agree" && wget "$telemetry_remover" && java -jar forge-1.7.10-10.13.4.1614-1.7.10-installer.jar --installServer && mv forge-1.7.10-10.13.4.1614-1.7.10-universal.jar forge.jar && rm forge-1.7.10-10.13.4.1614-1.7.10-installer.jar && mkdir mods && mkdir config
+    pacman -S jdk8-openjdk --noconfirm --needed && curl -LO "$pre_peak_minecraft_forge_server_jar" && curl -LO "$eula_auto_agree" && curl -LO "$telemetry_remover" && java -jar forge-1.7.10-10.13.4.1614-1.7.10-installer.jar --installServer && mv forge-1.7.10-10.13.4.1614-1.7.10-universal.jar forge.jar && rm forge-1.7.10-10.13.4.1614-1.7.10-installer.jar && mkdir mods && mkdir config
 elif [ "$choice" = "2" ]; then
-    pacman -S jdk8-openjdk --noconfirm --needed && wget "$peak_minecraft_forge_server_jar" && wget "$eula_auto_agree" && wget "$telemetry_remover" && java -jar forge-1.12.2-14.23.5.2860-installer.jar --installServer && mv forge-1.12.2-14.23.5.2860.jar forge.jar && rm forge-1.12.2-14.23.5.2860-installer.jar && mkdir mods && mkdir config
+    pacman -S jdk8-openjdk --noconfirm --needed && curl -LO "$peak_minecraft_forge_server_jar" && curl -LO "$eula_auto_agree" && curl -LO "$telemetry_remover" && java -jar forge-1.12.2-14.23.5.2860-installer.jar --installServer && mv forge-1.12.2-14.23.5.2860.jar forge.jar && rm forge-1.12.2-14.23.5.2860-installer.jar && mkdir mods && mkdir config
 elif [ "$choice" = "3" ]; then
-    pacman -S jdk17-openjdk --noconfirm --needed && wget "$newest_minecraft_server_jar" && wget "$eula_auto_agree" && wget "$telemetry_remover"
+    pacman -S jdk17-openjdk --noconfirm --needed && curl -LO "$newest_minecraft_server_jar" && curl -LO "$eula_auto_agree" && curl -LO "$telemetry_remover"
 fi
 
 chmod 777 /home/$USER/Minecraft_Server -R && reboot'
